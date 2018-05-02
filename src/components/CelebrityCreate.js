25b593dfb0cc846fb7b580193429621db35012ff
Khanh Nguyen <nguyenhuykhanh@gmail.com>
2018-05-03 00:42:17 +0700

    List celebrity component

---
+++ src/components/CelebrityCreate.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class CelebrityCreate extends React.Component {
+
+    render(){
+        return (<form>
+            <input/>
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+        </form>);
+    }
+}
+
+export default CelebrityCreate;
